7c3020b19a58f2ba6c6dc6db93513b7fde3fc901
ahmedMG21 <107784436+ahmedMG21@users.noreply.github.com>
2024-09-05 09:55:21 +0300

    -fix: note card expanding fixed

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,22 @@ import Note from "./components/Note";
 
 import notes from "./notes";
 
+// [ ] Implement the add note functionality.
+// [ ] Implement the delete note functionality.
+
 export default function App() {
   return (
     <div className="min-h-screen bg-zinc-200 flex flex-col">
       <Header />
 
       {/* <Note /> */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  mx-16 mt-5 gap-4 flex-grow">
-        {notes.map((note) => (
-          <Note key={note.key} title={note.title} content={note.content} />
-        ))}
-      </div>
+      <main className="flex-1 mx-16 mt-5 mb-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {notes.map((note) => (
+            <Note key={note.key} title={note.title} content={note.content} />
+          ))}
+        </div>
+      </main>
 
       <Footer />
     </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const currentYear = new Date().getFullYear();
 
 export default function Footer() {
   return (
-    <footer className="text-center h-16 flex items-center justify-center bg-white mt-4">
+    <footer className="text-center h-16 flex items-center justify-center mt-4">
       <p className="text-slate-400 text-base">Copyright ©️ {currentYear}</p>
     </footer>
   );
